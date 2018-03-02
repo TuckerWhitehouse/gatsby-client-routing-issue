401893bb32e94785788cf9b0f61ab31f7141e185
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2018-03-02 12:32:42 -0500

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+```bash
+npm install
+npm run build
+npm start
+
+open http://localhost:3000/some-custom-route
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+const root = __dirname
+
+app.get('/some-custom-route', (req, res) => {
+  res.sendFile('public/page1.html', { root })
+})
+
+app.get('/another-custom-route', (req, res) => {
+  res.sendFile('public/page2.html', { root })
+})
+
+app.use(express.static('public'))
+
+app.use((req, res) => res.sendFile('public/404.html', { root }))
+
+app.listen(3000, () => console.log('Listening on port 3000'))
